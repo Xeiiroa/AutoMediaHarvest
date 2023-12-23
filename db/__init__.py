72730db0b6a1528851ac5fcfc4056b0002eb52ea
Sintiria <Jaydentomlinson4@gmail.com>
2023-12-23 16:01:58 -0500

    Initialized database and database model

---
+++ db/__init__.py
@@ -0,0 +1,24 @@
+from os import path
+import os
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+DB_NAME = 'info.db' 
+engine = create_engine(f'sqlite:///{DB_NAME}', echo=True)
+
+def create_database():
+    if not path.exists("database/" + DB_NAME):
+        global engine
+        engine = create_engine(f'sqlite:///{DB_NAME}', echo=True)
+        
+        Base.metadata.create_all(engine)
+        
+        with Session(engine) as session:
+            default_settings = Settings()
+            session.add(default_settings)
+            session.commit()

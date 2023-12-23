958c95043b0709047090e16399c0ebf26a8ae8d1
Sintiria <Jaydentomlinson4@gmail.com>
2023-12-23 15:30:08 -0500

    swapped to python
    java had too many problems setting up that when asking for help nobody had a solution that worked

--- auto-media-harvest/src/main/db/Model.java
+++
@@ -1,7 +0,0 @@
-import io.github.cdimascio.dotenv.Dotenv;
-import Tools;
-
-public class Model {
-    String video_import_path = dotenv.get("DEFAULT_VIDEO_SAVEPATH");
-    String album_name ="Lifts";
-}

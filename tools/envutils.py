a8762ab9d835f411e00e2aa7638edce53a049169
Sintiria <Jaydentomlinson4@gmail.com>
2023-12-27 13:28:39 -0500

    updated envutils
    just moved text around

--- tools/envutils.py
+++ tools/envutils.py
@@ -1,10 +1,11 @@
+import os
+from dotenv import load_dotenv
 
-
-
-def load_variable(env_filename=".env", variable_name: str) -> str:
-    import os
-    from dotenv import load_dotenv
+def load_variable(variable_name, env_filename=".env"):
     load_dotenv(env_filename)
     return os.getenv(variable_name)
     
-    
+
+
+
+

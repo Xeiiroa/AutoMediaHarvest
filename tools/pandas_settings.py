af4bb8a2fac0a1d10306dd797ba7824a40907276
Sintiria <Jaydentomlinson4@gmail.com>
2023-12-31 13:51:56 -0500

    created pandas_settings.py
    created a tool file to auto use the settings making pandas returns easier to read

---
+++ tools/pandas_settings.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def pandas_settings():
+    pd.set_option("display.max_columns", 100)
+    pd.set_option("display.max_rows", 50)
+    pd.set_option("display.max_colwidth", 32)
+    pd.set_option("display.width", 120)
+    pd.set_option("expand_frame_repr", True)
+    
+
+if __name__ == "__main__":
+    pandas_settings()

44c0525f36bbbaf311e642ac12b2d63d1c569fd2
Sintiria <Jaydentomlinson4@gmail.com>
2023-12-31 15:05:51 -0500

    updated models.py
    removed unused functions in imports

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-from sqlalchemy import Column, String, Boolean, text
+from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -12,6 +12,6 @@ class Settings(Base):
     
     __tablename__ = "user_info"
     
-    id = Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     video_save_path: Mapped[str] = mapped_column(String, server_default=default_video_path + "/media")
     album_name: Mapped[str] = mapped_column(String, server_default="AutoMediaHarvest")

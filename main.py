a1a56601a4cda94804203b9639fce31275519c47
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-08 15:53:50 -0500

    Connected all endpoints

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from db import create_database
-from setup.app import create_app
-from setup.routes import include_routers
-from setup import prompt_permission
-
-list_of_routers= [
-    "routers.album.AlbumRouter"
-]
+from utils.albums import AlbumRouter as albums
+from utils.downloads import downloads as Downloads
+from config import Settings as Config
+from setup import AutoMediaHarvest as Script
 
 def main():
     create_database()
-    app = create_app()
-    include_routers(app, list_of_routers)
-    prompt_permission()
+    Config()
+    Script()
     
-    
-    
+        
 if __name__ == "__main__":
     main()

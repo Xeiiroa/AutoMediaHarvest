adfbd2640d4305bd931d0f11836dfb1db01ec739
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-16 19:22:08 -0500

    changed create database function
    changed create database function to work for sqlite instead of sqlalchemy

--- db/__init__.py
+++ db/__init__.py
@@ -1,24 +1,22 @@
 from os import path
 import os
-
+import sqlite3
 import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from .id_table import MediaIdTable, Base
+
+
 
 
 
 DB_NAME = 'mediaids.db' 
-engine = create_engine(f'sqlite:///{DB_NAME}', echo=True)
 
 def create_database():
-    if not path.exists('db/' + DB_NAME):
-        global engine
-        engine = create_engine(f'sqlite:///{DB_NAME}', echo=True)
-        
-        Base.metadata.create_all(engine)
-        
-        with Session(engine) as session:
-            default_settings = MediaIdTable()
-            session.add(default_settings)
-            session.commit()
+    global con
+    con = sqlite3.connect(DB_NAME)
+    cur = con.cursor()
+    try:
+        cur.execute('CREATE TABLE mediaids(ids)')
+    except sqlite3.OperationalError:
+        pass
+    
+    
+

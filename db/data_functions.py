3f5bf67bd06e7825db2c821b84bdd0eea7f6d3ec
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-17 13:27:49 -0500

    Created add id function remodel

--- db/data_functions.py
+++ db/data_functions.py
@@ -1,47 +1,18 @@
 import logging
-
-from sqlalchemy import select, Text, update, MetaData, Table, create_engine, insert
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
-
-from . import engine
-from .id_table import MediaIdTable
-
+import sqlite3
+from . import DB_NAME
 
 class Data:
-    def add_id(self, newId): #checks for duplicate ids and if no match is found adds newId to mediaId column in table
-        with Session(engine) as session:
-            try:
-                if not session.query(MediaIdTable).filter_by(mediaId=newId):
-                    session.execute(insert(MediaIdTable).values(mediaId=newId))
-                    session.commit()  
-            except SQLAlchemyError as e:
-                logging.error(f'A SQL error has occured when trying to add media Id to table: {str(e)}')
-                session.rollback()
-        
+    def __init__(self):
+        self.DB_NAME = str(DB_NAME)
+        self.con = sqlite3.connect(DB_NAME)
+        self.cur = self.con.cursor()
     
-    def clear_ids(self): #using an update statment clears all existing ids from database
-        with Session(engine) as session:
-            try:
-                clear = None
-                
-                session.execute(update(MediaIdTable).values(mediaId=clear))
-                session.commit()
-            except SQLAlchemyError as e:
-                logging.error(f'A SQL error has occured when trying to clear ids in table: {str(e)}')
-                session.rollback()
-                
-    def list_ids(self):
-       with Session(engine) as session:
-            try:
-                
-                ids = []
-                for media_id in session.query(MediaIdTable.mediaId).distinct():
-                    ids.append(media_id)
-                
-                return ids
-            
-            except SQLAlchemyError as e:
-                logging.error(f'A SQL error has occured when trying list ids from table: {str(e)}')
-                return None
-            
+    def add_id(self, Id:str):
+        try: 
+            self.cur.execute('INSERT INTO mediaids VALUES(?, ?)',(None, Id))
+            self.con.commit()
+            return 200
+        except Exception as e:
+            logging.error(f'A SQL error has occured when trying to add media Id to table: {str(e)}')
+            self.con.rollback()

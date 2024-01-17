ce8a6f48de9aa2a5ad3c2f8d0c304ba8b14f65df
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-17 18:25:46 -0500

    Created main function for script

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import argparse
+import sys
+import inspect
 from db import create_database
-from utils.albums import AlbumRouter as albums
-from utils.downloads import downloads as Downloads
 from config import Settings as Config
 from setup import AutoMediaHarvest as Script
+import logging
+
 
 def main():
     create_database()
-    Config()
-    Script()
+    parser = argparse.ArgumentParser(description='run script')
+    parser.add_argument('--settings', help='allows you to change settings', type=str) #value = None when called
+    args = parser.parse_args() 
     
+    if args.Config:
+        Config = Config()
         
+        try:
+            if hasattr(Config, args.Config):
+                getattr(Config, args.Config)()
+            else:
+                raise ValueError("Command does not exist")
+        except Exception as e:
+            logging.error(f'Command does not exist: {str(e)}')
+        sys.exit()
+    else:
+        Script()
+        
+    
 if __name__ == "__main__":
     main()
+
+        

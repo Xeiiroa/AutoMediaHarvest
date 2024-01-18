014195c3194cce3d896d3016f2bab61fdd6d3dd8
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-18 15:35:16 -0500

    save album media function

--- utils/downloads.py
+++ utils/downloads.py
@@ -3,19 +3,23 @@
 
 import os
 from tools.service import prompt_permission
-import requests 
+import requests #type: ignore
 import pandas as pd
-from db.data_functions import data as Data
-from .albums import AlbumRouter
+from db.data_functions import Data as Db
+from config import Settings as Config
+from .albums import AlbumRouter as Albums #type: ignore
+import logging
 
 
-class downloads(AlbumRouter):
+class Downloads(Albums): #check that everything that imports classes has the import statement with 'Downloads' capitalizsed
     def __init__(self):
-        self.savepath = Data.get_video_save_path()
-        self.service = prompt_permission()
-        self.album_id=self.search_album()
+        self.Config = Config()
+        self.savepath = self.Config.get_setting('videoSavepath')
+        self.service = prompt_permission() #may not be needed since it inherits from album router
+        self.album_id=self.search_album() #!subject to removal
     
-    def process_file(self, url:str, destination_folder:str, file_name:str):
+    def process_file(self, url:str, destination_folder:str, file_name:str): 
+        #* url acts as the place were content is grabbed (in case i forget)
         response = requests.get(url)
         if response.status_code == 200:
             print('Downloading file {0}'.format(file_name))
@@ -23,21 +27,38 @@ class downloads(AlbumRouter):
                 f.write(response.content)
                 f.close()
     
-    
-    
-    def save_media(self,):
-        media_files = self.service.mediaItems().search(body={})
         
-        for media_file in media_files:
+    def save_album_media(self, album_id:str): #add id save function to this
+        """
+        ___Possible errors to keep track of in this current version of this function___ 1/18/24
+        
+        - There is a chance that an album that has duplicates of the same item wont print an id for that item which can break the iterations of my current for loop
+        - But since its just for me i can follow the honor system
+        """
+        Data=Db() #? not sure if i have to make this a self argument ot not
+        media_files = self.service.mediaItems().search(body={'albumId': album_id}).execute()['mediaItems']
+        SavedMediaIds = Data.list_all_ids()
+        album_media_ids = [item['id'] for item in media_files]
+        
+        for iteration, media_file in enumerate(media_files):
             file_name=media_file['filename'] #* changes in v2: uses the medias id to check if the video has been downloaded before
             download_url = media_file['baseUrl'] + '=d'
-            """
-            conditional checking for if the file has already been downloaded,
+            mediafile_id = album_media_ids[iteration] #could use iter but this is what i know 
+            if mediafile_id in SavedMediaIds:
+                continue
             
-            - iterate over csv or database for the specific files media id 
-            - if found pass/continue
-            else run process file
-        
-            """
-            self.process_file(download_url, self.savepath, file_name)
+            try:
+                self.process_file(download_url, self.savepath, file_name)
+            except Exception as e:
+                logging.error(f'An error occured when trying to process files in save album media function: {str(e)}')
+                pass #! Remove pass when testing period is done
     
+    def dummy(self):
+        album_id = 'AIlsou33mbES7irrPbu8jEY_k7bpECzokOOt6VbO0mfOGhKbZluDRYoLRg3k2Kg_4sNOF7NueKZg'
+        Data = Db()
+        
+        media_files = self.service.mediaItems().search(body={'albumId': album_id}).execute()['mediaItems']
+        media_id = [item['id'] for item in media_files]
+        
+        print(media_id)
+        

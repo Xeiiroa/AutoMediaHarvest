ac766c300dac7c5fd9618de54780bdac3e856952
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-19 14:36:02 -0500

    Added timestamp function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from db import create_database
 from config import Settings as Config
 from setup.schedule import AutoMediaHarvest as Script
 import logging
+import datetime
+import os
 
 
 def main():
@@ -25,8 +27,17 @@ def main():
             logging.error(f'Command does not exist: {str(e)}')
         sys.exit()
     else:
+        #cwd = os.getcwd()
+        log_file = f'{os.getcwd()}/runtime_logs.txt'
+        timestamp(log_file)
         Script()
+
+
+def timestamp(filelocation:str):
+    with open(filelocation, 'a') as file:
+        file.write(f'{datetime.datetime.now()} - the script ran \n')
         
+    
     
 if __name__ == '__main__':
     main()

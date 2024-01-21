660f9e407aa9cb20db4b578e7eb5b78c70335fb8
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-21 15:05:35 -0500

    allowed service to be called on its own
    whenever a new key is needed i want the prompt permission function to be able to be called free of anything else since on occasion it saves the last function used and repeats it

--- tools/service.py
+++ tools/service.py
@@ -1,6 +1,7 @@
-from tools.envutils import load_variable
-from tools.Google import Create_Service # type: ignore
+from envutils import load_variable # type: ignore
+from Google import Create_Service # type: ignore
 import pandas
+#* may need to rework this in the case i need to create a new pickle file
 
 # function that requests access to users google photos account
 def prompt_permission():
@@ -14,4 +15,7 @@ def prompt_permission():
    
     service = Create_Service(google_client_secret, API_NAME, API_VERSION, SCOPES)
     return service
-    
+  
+if __name__ == '__main__':
+  prompt_permission()
+  

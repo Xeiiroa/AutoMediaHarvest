fcbb0a6ca0bedb5f47d393740ea6bc0478841950
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-21 14:59:25 -0500

    Cleanup and changes for windows task scheduler
    - removed timstamp function and moved it to its own file
    - removed excess calls
    - added notes for next test period of windows task scheduler

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import sys
 import inspect
 from db import create_database
 from config import Settings as Config
-from setup.schedule import AutoMediaHarvest as Script
+from setup.win_scheduler import AutoMediaHarvest as Script
 import logging
 import datetime
 import os
 from tools.error_report import write_error
+from tools.runtime_log import runtime_log
 
-
+create_database()
+log_file = f'{os.getcwd()}/logs/runtime_logs.txt'
 
 def main():
-    create_database()
     parser = argparse.ArgumentParser(description='run script')
     parser.add_argument('--Config', help='allows you to change settings', type=str) #value = None when called
     args = parser.parse_args() 
@@ -30,19 +31,21 @@ def main():
             logging.error(error_message)
         sys.exit()
     else:
-        #cwd = os.getcwd()
-        log_file = f'{os.getcwd()}/logs/runtime_logs.txt'
-        timestamp(log_file)
+        runtime_log(log_file)
         Script()
 
 
-def timestamp(filelocation:str):
-    with open(filelocation, 'a') as file:
-        file.write(f'{datetime.datetime.now()} - the script ran \n')
         
+def windows_scheduler():
+    Script()
+    runtime_log(log_file)
     
     
-if __name__ == '__main__':
-    main()
+    
+if __name__ == '__main__': #check for if the program runs without actually calling main and if so delete windows shceduler function
+    main()   
+"""else:
+    windows_scheduler()"""
+    
 
         

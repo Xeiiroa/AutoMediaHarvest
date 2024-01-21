1e13836a73426b647abad4d55854e363adc74eeb
Sintiria <Jaydentomlinson4@gmail.com>
2024-01-21 14:57:17 -0500

    Cleanup
    removed things that werent in use, and cleared and added notes

--- utils/downloads.py
+++ utils/downloads.py
@@ -7,7 +7,6 @@ import requests #type: ignore
 import pandas as pd
 from db.data_functions import Data as Db
 from config import Settings as Config
-from .albums import AlbumRouter as Albums #type: ignore
 import logging
 from tools.error_report import write_error
 
@@ -15,11 +14,12 @@ from tools.error_report import write_error
 
 class Downloads(): #check that everything that imports classes has the import statement with 'Downloads' capitalizsed
     def __init__(self):
-        self.Albums= Albums()
-        self.Config = Config()
-        self.savepath = self.Config.get_setting('videoSavepath')
+        Config = Config()
+        self.savepath = Config.get_setting('videoSavepath')
         self.service = prompt_permission() #may not be needed since it inherits from album router
-        self.album_id=self.Albums.search_album() #!subject to removal
+        
+        
+        
     
     def process_file(self, url:str, destination_folder:str, file_name:str): 
         #* url acts as the place were content is grabbed (in case i forget)
@@ -38,21 +38,23 @@ class Downloads(): #check that everything that imports classes has the import st
         - There is a chance that an album that has duplicates of the same item wont print an id for that item which can break the iterations of my current for loop
         - But since its just for me i can follow the honor system
         """
-        Data=Db() #? not sure if i have to make this a self argument ot not
+        Data=Db()
         media_files = self.service.mediaItems().search(body={'albumId': album_id}).execute()['mediaItems']
-        SavedMediaIds = Data.list_all_ids()
+        already_SavedMediaIds = Data.list_all_ids()
         album_media_ids = [item['id'] for item in media_files]
         
         for iteration, media_file in enumerate(media_files):
-            file_name=media_file['filename'] #* changes in v2: uses the medias id to check if the video has been downloaded before
+            file_name=media_file['filename']
             download_url = media_file['baseUrl'] + '=d'
             mediafile_id = album_media_ids[iteration] #could use iter but this is what i know 
-            if mediafile_id in SavedMediaIds:
+            if mediafile_id in already_SavedMediaIds:
                 continue
             
             try:
+                Data.add_id(album_media_ids[iteration])
                 self.process_file(download_url, self.savepath, file_name)
             except Exception as e:
                 error_message=f'An error occured when trying to process files in save album media function: {str(e)}'
                 logging.error(error_message)
+                write_error(error_message)
                 pass #! Remove pass when testing period is done
